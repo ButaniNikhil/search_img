4ed270b3726a9ddf2d777c2ff3cdf63a33b8182a
Nikhil Butani <nikhilbutani45@gmail.com>
2021-08-27 18:37:31 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
+import axios from 'axios'
 
 function App() {
+
+  let [pic, setimg] = useState([]);
+  let [page, setpage] = useState(1);
+
+  useEffect( () => {
+  
+    axios.get(`https://www.flickr.com/services/rest/?method=flickr.photos.getRecent&api_key=f67a4020861e0bc0a5f77916fc64a05e&per_page=500&page=1&format=json&nojsoncallback=1&auth_token=72157719734845313-3532daeebbad31b9&api_sig=fdc3109f48596a3c66029d421d6a62e2`).then((res)=>{
+      setimg(res.data.photos.photo)
+    })
+    },[]);
+
+    function changePhoto(){
+      setpage(page+1);
+    }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav>
+        <h1>Search image</h1>
+        <input type='search'></input>
+      </nav>
+      <div className='img_container'>
+          {pic.slice(0, page*50).map((photo,index) => {
+            
+            return(
+              <img src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_m.jpg`}></img>
+            )})}
+        </div>
+        <button onClick={changePhoto}>Show more</button>
     </div>
   );
 }
